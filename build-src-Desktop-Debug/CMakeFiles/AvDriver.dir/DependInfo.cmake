4c26f3bb0a902f48130a3a849892fdd700274ffc
krylov-mv@mail.ru <krylov-mv@mail.ru>
2025-04-24 10:54:43 +0500

    Обновил проект

---
+++ build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vmware/Проекты/Qt/AvDriver/src/industrialprotocolutils.cpp" "/home/vmware/Проекты/Qt/AvDriver/build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/industrialprotocolutils.cpp.o"
+  "/home/vmware/Проекты/Qt/AvDriver/src/main.cpp" "/home/vmware/Проекты/Qt/AvDriver/build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/main.cpp.o"
+  "/home/vmware/Проекты/Qt/AvDriver/src/modbustcpclient.cpp" "/home/vmware/Проекты/Qt/AvDriver/build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/modbustcpclient.cpp.o"
+  "/home/vmware/Проекты/Qt/AvDriver/src/opcuaclient.cpp" "/home/vmware/Проекты/Qt/AvDriver/build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/opcuaclient.cpp.o"
+  "/home/vmware/Проекты/Qt/AvDriver/src/rtm64var.cpp" "/home/vmware/Проекты/Qt/AvDriver/build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/rtm64var.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UA_ARCHITECTURE_POSIX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-src-Desktop-Debug/CMakeFiles/AvDriver.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "AvDriver"
+  "AvDriver.pdb"
+  "CMakeFiles/AvDriver.dir/industrialprotocolutils.cpp.o"
+  "CMakeFiles/AvDriver.dir/main.cpp.o"
+  "CMakeFiles/AvDriver.dir/modbustcpclient.cpp.o"
+  "CMakeFiles/AvDriver.dir/opcuaclient.cpp.o"
+  "CMakeFiles/AvDriver.dir/rtm64var.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AvDriver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
